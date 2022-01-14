0853d65831050a9dbb80907a347c5a3ce49d1c7f
Kikkirej <5462557+Kikkirej@users.noreply.github.com>
2022-01-14 14:02:27 +0100

    ✨ prevented installer from auto-closing

--- WinthainerInstaller/Program.cs
+++ WinthainerInstaller/Program.cs
@@ -14,6 +14,8 @@ namespace WinthainerInstaller
             Console.WriteLine("Installing distribution...");
             new DistributionInstaller().InstallDistribution();
             Console.WriteLine("Distribution installed!");
+            Console.WriteLine("Press any key to close...");
+            Console.ReadKey();
         }
     }
 }
